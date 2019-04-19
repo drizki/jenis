38676e3abd6704a09aa0bb0bc8e13b536dd798bd
yapie <targaryen.akane@gmail.com>
2019-04-19 19:06:09 +0700

    setup eslint-prettier

--- functions.js
+++ functions.js
@@ -1,51 +1,81 @@
 const functions = {
-    isString: object => typeof object === 'string' || object instanceof String,
-    isArray: object => object && typeof object === 'object' && object.constructor === Array,
-    isObject: object => object && typeof object === 'object' && object.constructor === Object,
-    isNumber: object => typeof object === 'number' && isFinite(object),
-    isFunction: object => typeof object === 'function',
-    isNull: object => object === null,
-    isUndefined: object => typeof object === 'undefined',
-    isBoolean: object => typeof object === 'boolean',
-    isRegExp: object => object && typeof object === 'object' && object.constructor === RegExp,
-    isError: object => object instanceof Error && typeof object.message !== 'undefined',
-    isDate: object => object instanceof Date,
-    isSymbol: object => typeof object === 'symbol',
-    isPromise: object => Object.prototype.toString.call(object) == 'promise' && object instanceof Promise,
-    check: object => {
-        switch (typeof object) {
-			case "number": return "number"
-			case "string": return "string"
-			case "boolean": return "boolean"
-			case "undefined": return "undefined"
-			case "function": return "function"
-			case "symbol": return "symbol"
-			case "object":
-			default:
-				switch (Object.prototype.toString.call(object)) {
-					case "[object Number]": return "number"
-					case "[object String]": return "string"
-					case "[object Boolean]": return "boolean"
-					case "[object Undefined]": return "undefined"
-					case "[object Null]": return "null"
-					case "[object Function]": return "function"
-					case "[object Array]": return "array"
-					case "[object Symbol]": return "symbolin"
-					case "[object Promise]": return "promise"
-					case "[object Error]": return "error"
-					case "[object RegExp]": return "regexp"
-					case "[object Date]": return "date"
-					default: 
-						if ( typeof Array !== "undefined" && object instanceof Array) return "array"
-						if ( typeof Function !== "undefined" && object instanceof Function) return "function"
-						if ( typeof Promise !== "undefined" && object instanceof Promise) return "promise"
-						if ( typeof Error !== "undefined" && object instanceof Error) return "error"
-						if ( typeof RegExp !== "undefined" && object instanceof RegExp) return "regexp"
-						if ( typeof Date !== "undefined" && object instanceof Date) return "date"
-				}
-		}
-		return "object"
-    },
-}
+  isString: object => typeof object === "string" || object instanceof String,
+  isArray: object =>
+    object && typeof object === "object" && object.constructor === Array,
+  isObject: object =>
+    object && typeof object === "object" && object.constructor === Object,
+  isNumber: object => typeof object === "number" && isFinite(object),
+  isFunction: object => typeof object === "function",
+  isNull: object => object === null,
+  isUndefined: object => typeof object === "undefined",
+  isBoolean: object => typeof object === "boolean",
+  isRegExp: object =>
+    object && typeof object === "object" && object.constructor === RegExp,
+  isError: object =>
+    object instanceof Error && typeof object.message !== "undefined",
+  isDate: object => object instanceof Date,
+  isSymbol: object => typeof object === "symbol",
+  isPromise: object =>
+    Object.prototype.toString.call(object) === "promise" &&
+    object instanceof Promise,
+  check: object => {
+    switch (typeof object) {
+      case "number":
+        return "number";
+      case "string":
+        return "string";
+      case "boolean":
+        return "boolean";
+      case "undefined":
+        return "undefined";
+      case "function":
+        return "function";
+      case "symbol":
+        return "symbol";
+      case "object":
+      default:
+        switch (Object.prototype.toString.call(object)) {
+          case "[object Number]":
+            return "number";
+          case "[object String]":
+            return "string";
+          case "[object Boolean]":
+            return "boolean";
+          case "[object Undefined]":
+            return "undefined";
+          case "[object Null]":
+            return "null";
+          case "[object Function]":
+            return "function";
+          case "[object Array]":
+            return "array";
+          case "[object Symbol]":
+            return "symbolin";
+          case "[object Promise]":
+            return "promise";
+          case "[object Error]":
+            return "error";
+          case "[object RegExp]":
+            return "regexp";
+          case "[object Date]":
+            return "date";
+          default:
+            if (typeof Array !== "undefined" && object instanceof Array)
+              return "array";
+            if (typeof Function !== "undefined" && object instanceof Function)
+              return "function";
+            if (typeof Promise !== "undefined" && object instanceof Promise)
+              return "promise";
+            if (typeof Error !== "undefined" && object instanceof Error)
+              return "error";
+            if (typeof RegExp !== "undefined" && object instanceof RegExp)
+              return "regexp";
+            if (typeof Date !== "undefined" && object instanceof Date)
+              return "date";
+        }
+    }
+    return "object";
+  }
+};
 
-module.exports = functions
+module.exports = functions;
--- index.js
+++ index.js
@@ -1,38 +1,41 @@
-(function (context) {
-    const f = require('./functions.js')
-    const fun = {
-        is: {
-            string:     o => f.isString(o),
-            array:      o => f.isArray(o),
-            object:     o => f.isObject(o),
-            number:     o => f.isNumber(o),
-            function:   o => f.isFunction(o),
-            null:       o => f.isNull(o),
-            undefined:  o => f.isUndefined(o),
-            boolean:    o => f.isBoolean(o),
-            regexp:     o => f.isRegExp(o),
-            error:      o => f.isError(o),
-            date:       o => f.isDate(o),
-            symbol:     o => f.isSymbol(o),
-            promise:    o => f.isPromise(o)
-        },
-        not: {
-            string:     o => !f.isString(o),
-            array:      o => !f.isArray(o),
-            object:     o => !f.isObject(o),
-            number:     o => !f.isNumber(o),
-            function:   o => !f.isFunction(o),
-            null:       o => !f.isNull(o),
-            undefined:  o => !f.isUndefined(o),
-            boolean:    o => !f.isBoolean(o),
-            regexp:     o => !f.isRegExp(o),
-            error:      o => !f.isError(o),
-            date:       o => !f.isDate(o),
-            symbol:     o => !f.isSymbol(o)
-        },
-        check: o => f.check(o)
-	}
-	if (typeof module !== 'undefined' && module.exports) module.exports = fun
-    else if (typeof define === "function" && define.amd) define("jenis", [], function () { return fun })
-    else context.jenis = fun
-})(this)
+(function(context) {
+  const f = require("./functions.js");
+  const fun = {
+    is: {
+      string: o => f.isString(o),
+      array: o => f.isArray(o),
+      object: o => f.isObject(o),
+      number: o => f.isNumber(o),
+      function: o => f.isFunction(o),
+      null: o => f.isNull(o),
+      undefined: o => f.isUndefined(o),
+      boolean: o => f.isBoolean(o),
+      regexp: o => f.isRegExp(o),
+      error: o => f.isError(o),
+      date: o => f.isDate(o),
+      symbol: o => f.isSymbol(o),
+      promise: o => f.isPromise(o)
+    },
+    not: {
+      string: o => !f.isString(o),
+      array: o => !f.isArray(o),
+      object: o => !f.isObject(o),
+      number: o => !f.isNumber(o),
+      function: o => !f.isFunction(o),
+      null: o => !f.isNull(o),
+      undefined: o => !f.isUndefined(o),
+      boolean: o => !f.isBoolean(o),
+      regexp: o => !f.isRegExp(o),
+      error: o => !f.isError(o),
+      date: o => !f.isDate(o),
+      symbol: o => !f.isSymbol(o)
+    },
+    check: o => f.check(o)
+  };
+  if (typeof module !== "undefined" && module.exports) module.exports = fun;
+  else if (typeof define === "function" && define.amd)
+    define("jenis", [], function() {
+      return fun;
+    });
+  else context.jenis = fun;
+})(this);
